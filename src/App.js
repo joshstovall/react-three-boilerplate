5e3626d53e0e17e7b6cea590a9f2542f8bb06773
Josh Stovall <40344766+joshstovall@users.noreply.github.com>
2022-09-23 20:28:56 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,75 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import Scene from './Scene';
 
 // class App extends Component {
 
 import text from './data/inputs/00.txt'; // Relative path to your File
-
+import './style.css'
 
 function App() {
 
-  const [inputs, setInputs] = useState('00');
+  const [inputs, setInputs] = useState({
+    satellites:[]
+  });
   const [outputs, setOutputs] = useState('00');
+  // const [data_tp, setOutputs] = useState('00');
+
+  const formatData = async (e) => {
+
+
+    let data  = []
+
+
+    // console.log('unformatted: ')
+    // console.log(e)
 
 
 
-  const updateTestData = (e) => {
+
+
+        let objs = e.split(/\r?\n/); // split at linebreaks, to array
+
+        // split_at_linebreak = 
+        objs.pop() // remove the last element (empty string)
+
+        // console.log("ARRAY YAY", objs)
+
+        
+
+
+for(let i=0;i<objs.length;i++){
+
+
+  let obj = objs[i]
+  let obj_to_arr = obj.split(' ') // split the string at ' ' spaces
+  let obj_json = {
+      id:parseInt(obj_to_arr[1]),
+      x:parseFloat(obj_to_arr[2]),
+      y:parseFloat(obj_to_arr[3]),
+      z:parseFloat(obj_to_arr[4])
+    
+  }
+  data.push(obj_json)
+  // data.push
+}
+
+
+        
+//     data = 
+//     [
+//   {
+//     id:1,
+//     x:0,
+//     y:0,
+//     z:0
+//   }
+// ]
+
+    return data
+  }
+
+
+  const updateTestData =async  (e) => {
 
 
     const a = e.target.value
@@ -21,29 +77,58 @@ function App() {
 
     fetch(`./data/inputs/${a}.txt`)
       .then(r => r.text())
-      .then(text => {
+      .then(async text => {
         console.log('text decoded:', text);
 
 
         // remove the comments from txt data
-        const withoutComments = text.replace(/^#.*\n?/gm, '');
+        const withoutComments = text.replace(/^#.*\n?/gm, '').replace(/(^[ \t]*\n)/gm, "");
 
-
-        console.log("widhout comments", withoutComments)
-
-
-
-        
         // format all satalites
-        const sats = withoutComments.replace(/^user.*\n?/gm, '');
+        const sats = withoutComments.replace(/^user.*\n?/gm, '').replace(/^interferer.*\n?/gm, '');
 
         // format all users
-        const users = withoutComments.replace(/^sat.*\n?/gm, '');
+        const users = withoutComments.replace(/^sat.*\n?/gm, '').replace(/^interferer.*\n?/gm, '');
 
+        // format all interferers
+        const interferers = withoutComments.replace(/^sat.*\n?/gm, '').replace(/^user.*\n?/gm, '');
 
 
         console.log("here are the sats", sats)
         console.log("here are the users", users)
+        console.log("here are the interferers", interferers)
+
+
+
+        // sataliate data structure
+        
+        
+
+
+        let formated_sats = await formatData(sats)
+        let formated_users = await formatData(users)
+        let formated_interferers = await formatData(interferers)
+
+console.log(formated_sats)
+
+
+let all_data = {
+  satellites:formated_sats,
+  users:formated_users,
+  interferers :formated_interferers
+}
+
+
+console.log('here is all the data')
+console.log(all_data)
+
+
+
+setInputs(all_data)
+
+
+
+
 
 
       });
@@ -52,6 +137,12 @@ function App() {
 
 
   }
+
+  useEffect(()=>{
+
+
+    console.log(inputs)
+  },setInputs)
 
   // render() {
   return (
@@ -70,10 +161,29 @@ function App() {
         <option value='10'>10</option>
         <option value='11'>11</option>
       </select>
-      <Scene testCase={inputs} />
+      <Scene
+       inputs={inputs ||{satellites:[]}} 
+       />
     </div>
   );
 }
 // }
 
 export default App;
+
+
+
+
+
+// function calcPosFromLatLonRad(lat,lon,radius){
+  
+//   var phi   = (90-lat)*(Math.PI/180);
+//   var theta = (lon+180)*(Math.PI/180);
+
+//   x = -(radius * Math.sin(phi)*Math.cos(theta));
+//   z = (radius * Math.sin(phi)*Math.sin(theta));
+//   y = (radius * Math.cos(phi));
+
+//   return [x,y,z];
+
+// }
